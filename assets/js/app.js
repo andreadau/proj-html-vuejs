5e5968769ead36af6e80b0bd5cf10bcac7202042
Andrea <dauriaandrea99@gmail.com>
2021-01-21 00:00:07 +0100

    Values !END!

--- assets/js/app.js
+++ assets/js/app.js
@@ -89,6 +89,28 @@ let vm = new Vue({
             {
                 img:"assets/img/project3-featured-189023420-400x400.jpg"
             },
-        ] 
+        ],
+        values: [
+            {
+                icon: "fas fa-home fa-3x bg-turq",
+                title: "Great Services",
+                text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda vel dolore nemo officiis magnam ipsam.",
+            },
+            {
+                icon: "fas fa-cog fa-3x bg-blue",
+                title: "Highest Standards",
+                text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda vel dolore nemo officiis magnam ipsam.",
+            },
+            {
+                icon: "fas fa-users fa-3x bg-orange",
+                title: "Professional Team",
+                text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda vel dolore nemo officiis magnam ipsam.",
+            },
+            {
+                icon: "fas fa-lightbulb fa-3x bg-green",
+                title: "Creative Solutions",
+                text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda vel dolore nemo officiis magnam ipsam.",
+            },
+        ]
     }
 });
